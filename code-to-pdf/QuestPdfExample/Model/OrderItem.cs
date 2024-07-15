895b2f384fbd040c438f09fd73338651cad72a2e
Md Rashid <mohdr138@gmail.com>
2024-07-15 18:00:50 +0530

    Add QuestPdf Invoice Example

---
+++ code-to-pdf/QuestPdfExample/Model/OrderItem.cs
@@ -0,0 +1,9 @@
+﻿namespace QuestPdfExample.Model
+{
+    public class OrderItem
+    {
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
+    }
+}
