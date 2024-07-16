aeeaa570e500a500177d7af2c8f7109555519e35
Md Rashid <mohdr138@gmail.com>
2024-07-16 12:39:49 +0530

    Add Puppeteer Sharp HTML to PDF example

---
+++ html-to-pdf/PuppeteerSharpExample/Program.cs
@@ -0,0 +1,28 @@
+﻿using PuppeteerSharp;
+using PuppeteerSharp.Media;
+
+//File path to your HTML script
+var html = File.ReadAllText("HTML_FILE_PATH");
+
+//Set pdf options
+var pdfOptions = new PdfOptions()
+{
+    Format = PaperFormat.A4
+};
+
+//Launch chromium instance
+using (var browser = await Puppeteer.LaunchAsync(new LaunchOptions
+{
+    //Set false for debugging
+    Headless = true,
+
+    //File path to your chrome exe
+    ExecutablePath = "CHROME_EXECUTABLE_PATH"
+}))
+{
+    using (var page = await browser.NewPageAsync())
+    {
+        await page.SetContentAsync(html);
+        await page.PdfAsync("invoice.pdf", pdfOptions);
+    }
+}
